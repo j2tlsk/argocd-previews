a395f04534721974f27ae3c0585f6041d8254dc4
Jan Lerski <jaler@miracle42.dk>
2024-05-08 10:02:51 +0200

    Added create.sh

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 APP_ID="${APP_ID:?Error: APP_ID must be set}"
 REPO="${REPO:?Error: REPO must be set}"
 IMAGE_TAG="${IMAGE_TAG:?Error: IMAGE_TAG must be set}"
-HOSTNAME="${HOST:?Error: HOST must be set}"
+HOSTNAME="${HOSTNAME:?Error: HOSTNAME must be set}"
 
 cat preview.yaml \
     | kyml tmpl -e REPO -e APP_ID -e IMAGE_TAG -e HOSTNAME \
