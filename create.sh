cc559710b2ae88bfd8555e94903c2a62901da0ca
Jan Lerski <56719740+j2tlsk@users.noreply.github.com>
2024-05-08 09:51:34 +0200

    Added create.sh

---
+++ create.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+APP_ID="${APP_ID:?Error: APP_ID must be set}"
+REPO="${REPO:?Error: REPO must be set}"
+IMAGE_TAG="${IMAGE_TAG:?Error: IMAGE_TAG must be set}"
+HOSTNAME="${HOST:?Error: HOST must be set}"
+
+cat preview.yaml \
+    | kyml tmpl -e REPO -e APP_ID -e IMAGE_TAG -e HOSTNAME \
+    | tee helm/templates/$APP_ID.yaml
